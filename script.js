0322410687fdd46da81abb1e0b01c1ca0a4f89f2
honeykhra <honeykhra@gmail.com>
2022-10-05 22:00:27 +0530

    Link list pseodo code added

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ class Node {
     this.next = null;
   }
 }
+
+// linkedlist class
+class LinkedList {
+	constructor()
+	{
+		this.head = null;
+		this.size = 0;
+	}
+
+	// functions to be implemented
+	// add(element)
+	// insertAt(element, location)
+	// removeFrom(location)
+	// removeElement(element)
+
+	// Helper Methods
+	// isEmpty
+	// size_Of_List
+	// PrintList
+}
